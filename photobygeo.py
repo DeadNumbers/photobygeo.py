cb44c2a0dac96bb3eb08476bf57e5bab82118db6
Betepok <BetepokNoname@gmail.com>
2015-04-15 10:07:56 -0400

    Version 1.01: Refactoring

--- photobygeo.py
+++ photobygeo.py
@@ -1,95 +1,110 @@
-import httplib
-import urllib
-import json
-import datetime
+"""
+Author: 	Betepok
+Version:	1.01
+Date:		15.04.2015
+Twitter:	http://twitter.com/BetepO_ok
 
-location_latitude = '55.740701'
-location_longitude = '37.609161'
-distance = '100'
-min_timestamp = 1400619600
-max_timestamp = 1400792400
-date_increment = 60*60*3 # every 3 hours
-instagram_access_token = 'YOUR_INSTAGRAM_TOKEN'
+"""
 
-def get_instagram(latitude, longitude, distance, min_timestamp, max_timestamp, access_token):
-	get_request =  '/v1/media/search?lat=' + latitude
-	get_request	+= '&lng=' + longitude
-	get_request += '&distance=' + distance
-	get_request += '&min_timestamp=' + str(min_timestamp)
-	get_request += '&max_timestamp=' + str(max_timestamp)
-	get_request += '&access_token=' + access_token
-	local_connect = httplib.HTTPSConnection('api.instagram.com', 443)
-	local_connect.request('GET', get_request)
-	return local_connect.getresponse().read()
+from datetime import datetime
+from requests import get as getResponse
 
-def get_vk(latitude, longitude, distance, min_timestamp, max_timestamp):
-	get_request =  '/method/photos.search?lat=' + location_latitude
-	get_request	+= '&long=' + location_longitude
-	get_request	+= '&count=100'
-	get_request	+= '&radius=' + distance
-	get_request	+= '&start_time=' + str(min_timestamp)
-	get_request	+= '&end_time=' + str(max_timestamp)
-	local_connect = httplib.HTTPSConnection('api.vk.com', 443)
-	local_connect.request('GET', get_request)
-	return local_connect.getresponse().read()
 
-def timestamptodate(timestamp):
-	return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')+' UTC'
+LOCATION_LATITUDE = '55.740701'
+LOCATION_LONGITUDE = '37.609161'
+DISTANCE = '100'
+MIN_TIMESTAMP = 1400619600
+MAX_TIMESTAMP = 1400792400
+DATE_INCREMENT = 60*60*24
+INSTAGRAM_ACCESS_TOKEN = 'YOUR_INSTAGRAM_TOKEN'
 
-def parse_instagram(location_latitude, location_longitude, distance, min_timestamp, max_timestamp, date_increment, instagram_access_token):
-	if instagram_access_token == 'YOUR_INSTAGRAM_TOKEN':
+
+def getInstagram(latitude, longitude, distance, minTimestamp, maxTimestamp):
+	params = {
+		'lat': latitude,
+		'lng': longitude,
+		'distance': distance,
+		'min_timestamp': str(minTimestamp),
+		'max_timestamp': str(maxTimestamp),
+		'access_token': INSTAGRAM_ACCESS_TOKEN
+	}
+	return getResponse("https://api.instagram.com/v1/media/search", \
+		params=params, verify=True).json()
+
+
+def getVK(latitude, longitude, distance, minTimestamp, maxTimestamp):
+	params = {
+		'lat': latitude,
+		'long': longitude,
+		'count': '100',
+		'radius': distance,
+		'start_time': minTimestamp,
+		'end_time': maxTimestamp,
+	}
+	return getResponse("https://api.vk.com/method/photos.search", \
+		params=params, verify=True).json()
+
+
+def convertTSToDate(timestamp):
+	return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S') + ' UTC'
+
+
+def parseInstagram(latitude, longitude, distance, minTimestamp, maxTimestamp, dateIncrement):
+	if INSTAGRAM_ACCESS_TOKEN == 'YOUR_INSTAGRAM_TOKEN':
 		print 'You should add your instagram access token'
 		return
 	print 'Starting parse instagram..'
-	print 'GEO:',location_latitude,location_longitude
-	print 'TIME: from',timestamptodate(min_timestamp),'to',timestamptodate(max_timestamp)
-	file_inst = open('instagram_'+location_latitude+location_longitude+'.html','w')
-	file_inst.write('<html>')
-	local_min_timestamp = min_timestamp
+	print 'GEO:', latitude, longitude
+	print 'TIME: from', convertTSToDate(minTimestamp), 'to', convertTSToDate(maxTimestamp)
+	fileDescriptor = open('instagram_' + latitude + longitude + '.html', 'w')
+	fileDescriptor.write('<html>')
+	localMinTimestamp = minTimestamp
 	while (1):
-		if ( local_min_timestamp >= max_timestamp ):
+		if ( localMinTimestamp >= maxTimestamp ):
 			break
-		local_max_timestamp = local_min_timestamp + date_increment
-		if ( local_max_timestamp > max_timestamp ):
-			local_max_timestamp = max_timestamp
-		print timestamptodate(local_min_timestamp),'-',timestamptodate(local_max_timestamp)
-		instagram_json = json.loads(get_instagram(location_latitude, location_longitude, distance, local_min_timestamp, local_max_timestamp, instagram_access_token))
-		for local_i in instagram_json['data']:
-			file_inst.write('<br>')
-			file_inst.write('<img src='+local_i['images']['standard_resolution']['url']+'><br>')
-			file_inst.write(timestamptodate(int(local_i['created_time']))+'<br>')
-			file_inst.write(local_i['link']+'<br>')
-			file_inst.write('<br>')
-		local_min_timestamp = local_max_timestamp
-	file_inst.write('</html>')
-	file_inst.close()
+		localMaxTimestamp = localMinTimestamp + dateIncrement
+		if ( localMaxTimestamp > maxTimestamp ):
+			localMaxTimestamp = maxTimestamp
+		print convertTSToDate(localMinTimestamp), '-', convertTSToDate(localMaxTimestamp)
+		responseJSON = getInstagram(latitude, longitude, distance, localMinTimestamp, localMaxTimestamp)
+		for fieldJSON in responseJSON['data']:
+			fileDescriptor.write('<br>')
+			fileDescriptor.write('<img src=' + fieldJSON['images']['standard_resolution']['url'] + '><br>')
+			fileDescriptor.write(convertTSToDate(int(fieldJSON['created_time'])) + '<br>')
+			fileDescriptor.write(fieldJSON['link'] + '<br>')
+			fileDescriptor.write('<br>')
+		localMinTimestamp = localMaxTimestamp
+	fileDescriptor.write('</html>')
+	fileDescriptor.close()
 
-def parse_vk(location_latitude, location_longitude, distance, min_timestamp, max_timestamp, date_increment):
+
+def parseVK(latitude, longitude, distance, minTimestamp, maxTimestamp, dateIncrement):
 	print 'Starting parse vkontakte..'
-	print 'GEO:',location_latitude,location_longitude
-	print 'TIME: from',timestamptodate(min_timestamp),'to',timestamptodate(max_timestamp)
-	file_inst = open('vk_'+location_latitude+location_longitude+'.html','w')
-	file_inst.write('<html>')
-	local_min_timestamp = min_timestamp
+	print 'GEO:', latitude, longitude
+	print 'TIME: from', convertTSToDate(minTimestamp), 'to', convertTSToDate(maxTimestamp)
+	fileDescriptor = open('vk_' + latitude + longitude + '.html', 'w')
+	fileDescriptor.write('<html>')
+	localMinTimestamp = minTimestamp
 	while (1):
-		if ( local_min_timestamp >= max_timestamp ):
+		if (localMinTimestamp >= maxTimestamp):
 			break
-		local_max_timestamp = local_min_timestamp + date_increment
-		if ( local_max_timestamp > max_timestamp ):
-			local_max_timestamp = max_timestamp
-		print timestamptodate(local_min_timestamp),'-',timestamptodate(local_max_timestamp)
-		vk_json = json.loads(get_vk(location_latitude, location_longitude, distance, local_min_timestamp, local_max_timestamp))
-		for local_i in vk_json['response']:
-			if type(local_i) is int:
+		localMaxTimestamp = localMinTimestamp + dateIncrement
+		if (localMaxTimestamp > maxTimestamp):
+			localMaxTimestamp = maxTimestamp
+		print convertTSToDate(localMinTimestamp), '-', convertTSToDate(localMaxTimestamp)
+		responseJSON = getVK(latitude, longitude, distance, localMinTimestamp, localMaxTimestamp)
+		for fieldJSON in responseJSON['response']:
+			if type(fieldJSON) is int:
 				continue
-			file_inst.write('<br>')
-			file_inst.write('<img src='+local_i['src_big']+'><br>')
-			file_inst.write(timestamptodate(int(local_i['created']))+'<br>')
-			file_inst.write('http://vk.com/id'+str(local_i['owner_id'])+'<br>')
-			file_inst.write('<br>')
-		local_min_timestamp = local_max_timestamp
-	file_inst.write('</html>')
-	file_inst.close()
+			fileDescriptor.write('<br>')
+			fileDescriptor.write('<img src=' + fieldJSON['src_big'] + '><br>')
+			fileDescriptor.write(convertTSToDate(int(fieldJSON['created'])) + '<br>')
+			fileDescriptor.write('http://vk.com/id' + str(fieldJSON['owner_id']) + '<br>')
+			fileDescriptor.write('<br>')
+		localMinTimestamp = localMaxTimestamp
+	fileDescriptor.write('</html>')
+	fileDescriptor.close()
 
-parse_instagram(location_latitude, location_longitude, distance, min_timestamp, max_timestamp, date_increment, instagram_access_token)
-parse_vk(location_latitude, location_longitude, distance, min_timestamp, max_timestamp, date_increment)
+
+parseInstagram(LOCATION_LATITUDE, LOCATION_LONGITUDE, DISTANCE, MIN_TIMESTAMP, MAX_TIMESTAMP, DATE_INCREMENT)
+parseVK(LOCATION_LATITUDE, LOCATION_LONGITUDE, DISTANCE, MIN_TIMESTAMP, MAX_TIMESTAMP, DATE_INCREMENT)
